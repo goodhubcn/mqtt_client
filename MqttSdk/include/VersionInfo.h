0a0d53567f5c50ed9bbaa8a69089e65eaf9056e2
jackiema <jackiema@tinnove.com.cn>
2026-06-29 14:21:21 +0800

    Add mqtt client

---
+++ MqttSdk/include/VersionInfo.h
@@ -0,0 +1,17 @@
+/*******************************************************************************
+ * Version information for Eclipse Paho MQTT C.
+ *******************************************************************************/
+
+#ifndef VERSIONINFO_H
+#define VERSIONINFO_H
+
+#define BUILD_TIMESTAMP "2026-03-04T02:53:36Z"
+#define CLIENT_VERSION  "1.3.15"
+
+#define PAHO_MQTT_C_VERSION  "1.3.15"
+
+#define PAHO_MQTT_C_VERSION_MAJOR  1
+#define PAHO_MQTT_C_VERSION_MINOR  3
+#define PAHO_MQTT_C_VERSION_PATCH  15
+ 
+#endif /* VERSIONINFO_H */
